489cfec94e4655d9229a748121bd4f8c54534933
Ameer Aftab <musaajmal57@gmail.com>
2025-10-30 01:35:49 +0500

    docker images working

---
+++ backend/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+# # DATABASE_URL = os.getenv("DATABASE_URL")
+
+# if not DATABASE_URL:
+#     raise ValueError("No DATABASE_URL set in environment variables")
+
+engine = create_engine("postgresql://avnadmin:AVNS_b1AYuNU7LHng_BIqa0U@pizza-delivery-api-musaajmal57-6bf8.g.aivencloud.com:11389/defaultdb")
+
+Base=declarative_base()
+
+Session=sessionmaker()
