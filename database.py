787fe5decd1069d4339dc76213143cf21008a30f
Ameer Aftab <musaajmal57@gmail.com>
2025-10-29 23:19:39 +0500

    fixed

--- database.py
+++ database.py
@@ -2,9 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
 
-engine=create_engine('postgresql://avnadmin:AVNS_b1AYuNU7LHng_BIqa0U@pizza-delivery-api-musaajmal57-6bf8.g.aivencloud.com:11389/defaultdb',
-    echo=True
-)
+import os
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("No DATABASE_URL set in environment variables")
+
+engine = create_engine(DATABASE_URL)
 
 Base=declarative_base()
 
